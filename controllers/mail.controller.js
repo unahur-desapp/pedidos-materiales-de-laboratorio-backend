ff4e451ee0948de919e755447a44a4803d8a5622
claudio Molina <molina.claudio@estudiantes.unahur.edu.ar>
2023-11-25 16:51:57 -0300

    socket back

--- controllers/mail.controller.js
+++ controllers/mail.controller.js
@@ -4,7 +4,7 @@ const Mail = require("../models/mail.js");
 //Post de un material
 module.exports.getMails = async (req, res) => {
     try {
-        const data = await Mail.findById(req.params.id)
+        const data = await Mail.findById(req.params.id_pedido)
         return res.json(data);
     } catch (error) {
         return res.status(400).json({ message: error.message });
@@ -14,9 +14,10 @@ module.exports.getMails = async (req, res) => {
 module.exports.sendMail = async (req, res) => {
     console.log(req.body) 
     try {
-        const updatedData = req.body;   
-        
-        const data = new Mail(updatedData);  
+        const id = req.body.id_pedido;
+        const mail = req.body.mail;
+        const data = await Mail.findById(id)
+        data.list_mensajes.push(mail)
         const dataToSave = await data.save();
         return res.status(200).json(dataToSave);
     } catch (error) {
--- models/mail.js
+++ models/mail.js
@@ -1,23 +1,28 @@
 const mongoose = require("mongoose");
 
 const dataSchema = new mongoose.Schema({
-  nombre: {
-    type: String,
-  },
-  id_emisor: {
-    required: true,
-    type: String,
-  },
-  mensaje: {
-    required: true,
-    type: String,
-  },
-  read: {
-    type: Boolean,
-  },
+  
   id_pedido: {
     type: String,
   },
+  list_mensajes :[
+      {
+      nombre: {
+        type: String,
+      },
+      id_emisor: {
+        required: true,
+        type: String,
+      },
+      mensaje: {
+        required: true,
+        type: String,
+      },
+      read: {
+        type: Boolean,
+      }
+    }
+  ]
 });
 
 module.exports = mongoose.model("Mail", dataSchema);
