d0486a9f05aa59812250e4ab0ba95fe6a3800532
claudio Molina <molina.claudio@estudiantes.unahur.edu.ar>
2024-04-25 20:38:22 -0300

    se fixea error en dailyUpdate de pedidos

--- common/dailyValidityCheck.js
+++ common/dailyValidityCheck.js
@@ -2,7 +2,7 @@ const cron = require('node-cron');
 const updateValidity = require('./updateValidity'); // importa tu función
 
 // Ejecuta la función todos los días a la medianoche
-const dailyUpdate = cron.schedule('0 0 * * *', () => {D
+const dailyUpdate = cron.schedule('0 0 * * *', () => {
     updateValidity();
 });
 
